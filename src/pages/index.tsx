55ce90b60fd99aced58b73112a5d676dd0107214
02kK <lahass1213@gmail.com>
2022-07-18 00:32:53 +0900

    sass化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 import WeatherMap from "./WeatherMap";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/Home.module.sass";
 
 export default function Home() {
   return (
