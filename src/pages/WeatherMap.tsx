3c729aaa74f3cd8c56c2291b939d22ab8cd4ae3e
queuek <lahass1213@gmail.com>
2022-07-31 10:24:52 +0900

    不要なJSXを削除

--- src/pages/WeatherMap.tsx
+++ src/pages/WeatherMap.tsx
@@ -1,13 +1,15 @@
 const WeatherMap = () => {
   return (
     <div className="weather-map">
-      <p>地上天気図(日本域)</p>
       <img
         src="http://net.cive.gifu-u.ac.jp/pic/zumen/analysis/chart1001.jpg"
         alt="地上天気図(日本域)"
         width="500px"
         height="500px"
       />
+      <p style={{ fontSize: "20px", textAlign: "center", color: "#55f" }}>
+        地上天気図(日本域)
+      </p>
     </div>
   );
 };
